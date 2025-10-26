d246e3e986b3250c7c6ac12127ab784203835abe
Matthew Foley <matthewfoley333@gmail.com>
2025-10-27 00:16:17 +1300

    feat: add styling and delete commented out code

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,41 @@
+// import Map from './Map.tsx'
+// import { useState } from 'react'
+// import PredictionOverlay from './PredictionOverlay.tsx'
+
+// function App() {
+//   const [selectedTeam, setSelectedTeam] = useState<string | null>(null)
+//   return (
+//     <>
+//       <div className="app">
+//         <Map onTeamClick={(name) => setSelectedTeam(name)} />
+//       </div>
+
+//       {selectedTeam && (
+//         <PredictionOverlay
+//           teamName={selectedTeam}
+//           onClose={() => setSelectedTeam(null)}
+//         />
+//       )}
+//     </>
+//   )
+// }
+
+// export default App
+
 import Map from './Map.tsx'
 import { useState } from 'react'
 import PredictionOverlay from './PredictionOverlay.tsx'
 
 function App() {
   const [selectedTeam, setSelectedTeam] = useState<string | null>(null)
+
   return (
     <>
       <div className="app">
-        <Map onTeamClick={(name) => setSelectedTeam(name)} />
+        <Map
+          onTeamClick={(name) => setSelectedTeam(name)}
+          isOverlayOpen={!!selectedTeam}
+        />
       </div>
 
       {selectedTeam && (
--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -1,4 +1,104 @@
-// import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+// // import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+// import { useEffect, useRef } from 'react'
+// import tt from '@tomtom-international/web-sdk-maps'
+// import { useTeams } from '../hooks/teams.ts'
+// import { Link } from 'react-router'
+// import '@tomtom-international/web-sdk-maps/dist/maps.css'
+
+// const containerStyle = { width: '100%', height: '1000px' }
+// const center = { lng: -2.0, lat: 54.0 }
+
+// function Map({
+//   onTeamClick,
+//   isOverlayOpen,
+// }: {
+//   onTeamClick: (name: string) => void
+//   isOverlayOpen: boolean
+// }) {
+//   const mapRef = useRef<HTMLDivElement | null>(null)
+//   const apiKey = import.meta.env.VITE_TOMTOM_API_KEY
+
+//   const {
+//     data: teams,
+//     isLoading: isTeamsLoading,
+//     isError: isTeamsError,
+//   } = useTeams()
+//   useEffect(() => {
+//     if (!mapRef.current || !teams) return
+
+//     const map = tt.map({
+//       key: apiKey,
+//       container: mapRef.current,
+//       center,
+//       zoom: 6,
+//     })
+
+//     const createdMarkers: tt.Marker[] = []
+
+//     teams.forEach((team) => {
+//       if (!team.stadium_lat || !team.stadium_lng) return
+
+//       const el = document.createElement('div')
+//       el.style.width = '40px'
+//       el.style.height = '40px'
+
+//       const img = document.createElement('img')
+//       img.src = team.team_logo
+//       img.style.width = '100%'
+//       img.style.height = '100%'
+//       img.style.borderRadius = '50%'
+//       img.style.cursor = 'pointer'
+//       img.style.display = 'block'
+//       img.style.border = '2px solid white'
+
+//       el.appendChild(img)
+
+//       const marker = new tt.Marker({ element: el, anchor: 'center' })
+//         .setLngLat([team.stadium_lng, team.stadium_lat])
+//         .addTo(map)
+
+//       el.addEventListener('click', () => {
+//         onTeamClick(team.team_name)
+//       })
+
+//       createdMarkers.push(marker)
+//     })
+
+//     return () => {
+//       createdMarkers.forEach((marker) => marker.remove())
+//       map.remove()
+//     }
+//   }, [teams, apiKey])
+
+//   if (isTeamsLoading) {
+//     return <div>...Loading</div>
+//   }
+
+//   if (isTeamsError) {
+//     return <div>Error loading teams</div>
+//   }
+
+//   return (
+//     <div ref={mapRef} style={containerStyle}>
+//       <div ref={mapRef} style={containerStyle}>
+//         {!isOverlayOpen && (
+//           <Link to="/">
+//             {' '}
+//             <img src="/images/back.png" alt="Back" className="back-button" />
+//           </Link>
+//         )}
+//         <img
+//           src="/images/match-model.png"
+//           alt="pl- prediction-logo"
+//           className="pl-logo"
+//         />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Map
+
 import { useEffect, useRef } from 'react'
 import tt from '@tomtom-international/web-sdk-maps'
 import { useTeams } from '../hooks/teams.ts'
@@ -8,7 +108,13 @@ import '@tomtom-international/web-sdk-maps/dist/maps.css'
 const containerStyle = { width: '100%', height: '1000px' }
 const center = { lng: -2.0, lat: 54.0 }
 
-function Map({ onTeamClick }: { onTeamClick: (name: string) => void }) {
+function Map({
+  onTeamClick,
+  isOverlayOpen,
+}: {
+  onTeamClick: (name: string) => void
+  isOverlayOpen: boolean
+}) {
   const mapRef = useRef<HTMLDivElement | null>(null)
   const apiKey = import.meta.env.VITE_TOMTOM_API_KEY
 
@@ -17,6 +123,7 @@ function Map({ onTeamClick }: { onTeamClick: (name: string) => void }) {
     isLoading: isTeamsLoading,
     isError: isTeamsError,
   } = useTeams()
+
   useEffect(() => {
     if (!mapRef.current || !teams) return
 
@@ -74,17 +181,18 @@ function Map({ onTeamClick }: { onTeamClick: (name: string) => void }) {
 
   return (
     <div ref={mapRef} style={containerStyle}>
-      <div ref={mapRef} style={containerStyle}>
-        <Link to="/">
-          {' '}
-          <img src="/images/back.png" alt="Back" className="back-button" />
-        </Link>
-        <img
-          src="/images/match-model.png"
-          alt="pl- prediction-logo"
-          className="pl-logo"
-        />
-      </div>
+      {!isOverlayOpen && (
+        <>
+          <Link to="/">
+            <img src="/images/back.png" alt="Back" className="back-button" />
+          </Link>
+          <img
+            src="/images/match-model.png"
+            alt="pl- prediction-logo"
+            className="pl-logo"
+          />
+        </>
+      )}
     </div>
   )
 }
